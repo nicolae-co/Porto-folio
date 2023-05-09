5e83acf518ba829dc26a5814806e80dff909231b
nicolae-co <cojocarugithub@gmail.com>
2023-05-09 11:30:15 +0300

    Added nav tabs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 const btnLight = document.querySelector(".btn__light");
 const btnDark = document.querySelector(".btn__dark");
 
+const navBtns = document.querySelectorAll(".nav__btn");
+const navContainer = document.querySelector(".nav");
+const navContent = document.querySelectorAll(".content__tab");
+
 const body = document.querySelector("body");
 
 function toggleMode(mode) {
@@ -22,6 +26,26 @@ btnDark.addEventListener("click", function () {
 });
 
 // console.log((btnDark.childNodes[1].style.backgroundColor = "white"));
+
+// Implementing Nav Component
+navContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".nav__btn");
+  if (!clicked) return;
+
+  // Remove active btn
+  navBtns.forEach((b) => b.classList.remove("nav__btn--active"));
+  navContent.forEach((c) => c.classList.remove("content__tab--active"));
+
+  // Activate btn
+  clicked.classList.add("nav__btn--active");
+
+  // Activate content area
+  document
+    .querySelector(`.content__${clicked.dataset.nav}`)
+    .classList.add("content__tab--active");
+
+  console.log("test");
+});
 
 window.addEventListener("load", function () {
   btnLight.childNodes[1].style.backgroundColor = "white";
