0957dc5bd71506a04a973fac0525e76abfc943d5
nicolae-co <cojocarugithub@gmail.com>
2023-05-10 13:51:51 +0300

    added menu animations

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const btnLight = document.querySelector(".btn__light");
 const btnDark = document.querySelector(".btn__dark");
 
@@ -6,31 +7,61 @@ const navBtns = document.querySelectorAll(".nav__btn");
 const navContainer = document.querySelector(".nav");
 const navContent = document.querySelectorAll(".content__tab");
 
+const contactContent = document.querySelector(".content__4");
+
 const body = document.querySelector("body");
+
+// Theme mode (dark/light)
 
 function toggleMode(mode) {
   body.classList.remove("lightMode", "darkMode");
   body.classList.add(mode);
 }
 
+function setButtonStyle(button, backgroundColor) {
+  button.childNodes[1].style.backgroundColor = backgroundColor;
+}
+
 btnLight.addEventListener("click", function () {
   toggleMode("lightMode");
-  btnDark.childNodes[1].style.backgroundColor = "";
-  btnLight.childNodes[1].style.backgroundColor = "white";
+  setButtonStyle(btnDark, "");
+  setButtonStyle(btnLight, "white");
 });
 
 btnDark.addEventListener("click", function () {
   toggleMode("darkMode");
-  btnDark.childNodes[1].style.backgroundColor = "white";
-  btnLight.childNodes[1].style.backgroundColor = "";
+  setButtonStyle(btnDark, "white");
+  setButtonStyle(btnLight, "");
 });
 
 // console.log((btnDark.childNodes[1].style.backgroundColor = "white"));
 
+const generateContantLinks = function (display) {
+  if (contactContent) {
+    contactContent.innerHTML = `
+    <a href="https://www.instagram.com/iamnicolaescu/?hl=en" target="_blank">
+      <img class="logo" src="./src/icons/instagramLogo.svg"/>
+      Instagram
+    </a>
+    <a href="https://www.linkedin.com/in/nicolae-cojocaru-532119233/" target="_blank">
+      <img class="logo" src="./src/icons/linkedInLogo.svg"/>
+      LinkedIn
+    </a>
+    <a href="https://github.com/nicolae-co" target="_blank">
+      <img class="logo" src="./src/icons/githubLogo.svg"/>
+      Github
+    </a>
+    `;
+    contactContent.style.display = display;
+  }
+};
 // Implementing Nav Component
 navContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".nav__btn");
   if (!clicked) return;
+  clicked.dataset.nav === "4"
+    ? generateContantLinks("flex")
+    : generateContantLinks("none");
 
   // Remove active btn
   navBtns.forEach((b) => b.classList.remove("nav__btn--active"));
@@ -43,8 +74,6 @@ navContainer.addEventListener("click", function (e) {
   document
     .querySelector(`.content__${clicked.dataset.nav}`)
     .classList.add("content__tab--active");
-
-  console.log("test");
 });
 
 window.addEventListener("load", function () {
